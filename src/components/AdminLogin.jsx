6cdd2996857c24beff430e0ede6fad48de68f480
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-11-13 14:26:05 +0800

    Update API endpoint for admin login

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -8,7 +8,7 @@ function AdminLogin() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/admin/login", {
+    const res = await fetch("https://mood-tracker-5.onrender.com//api/admin/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
