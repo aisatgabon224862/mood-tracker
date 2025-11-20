f07bc36d7d1fcd7a0c764f979d85483a2d956b40
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-11-20 11:35:43 +0800

    Change header from h2 to h3 and adjust textarea rows

--- src/components/MoodForm.jsx
+++ src/components/MoodForm.jsx
@@ -36,7 +36,7 @@ function MoodForm() {
     <>
       <div className="d-flex justify-content-between align-items-center header-row ">
         <img src={logo} alt="logo" className="logos mt-1" />
-        <h2 className="tropical mb-5">TROPICAL VILLAGE NATIONAL HIGH SCHOOL</h2>
+        <h3 className="tropical mb-5">TROPICAL VILLAGE NATIONAL HIGH SCHOOL</h3>
         <img src={deped} alt="deped logo" className="deped " />
       </div>
       <div className="form-container">
@@ -76,7 +76,7 @@ function MoodForm() {
             <textarea
               name="explanation"
               placeholder="Explain your feelings..."
-              rows="4"
+              rows="1"
               onChange={handleChange}
               required
             ></textarea>
