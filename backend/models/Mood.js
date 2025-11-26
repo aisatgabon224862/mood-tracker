e60dd9202779b95d0a81edfad8a60b87394ea4fc
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-11-26 12:21:21 +0800

    Add grade field to mood schema

--- backend/models/Mood.js
+++ backend/models/Mood.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const moodSchema = new mongoose.Schema({
   name: { type: String, required: true },
   section: { type: String, required: true },
+  grade: { type: String, required: true },
   emotion: { type: String, required: true },
   explanation: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
