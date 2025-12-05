53978916bd80157b671f38f6ab9f5e27dbca9c4d
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-12-05 15:08:41 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import adminRoutes from "./routes/adminroutes.js";
+import exportRoutes from "./routes/exportRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 //  Admin routes
 app.use("/api/admin", adminRoutes);
+app.use("/api", exportRoutes);
 
 //  Mood Schema
 const moodSchema = new mongoose.Schema({
