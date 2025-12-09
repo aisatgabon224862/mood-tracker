df7a4f04faa98468a62c2714d058dbf646fcc4d1
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-12-09 14:03:44 +0800

    Configure CORS for specific origin and methods

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,15 @@ import Mood from "./models/Mood.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mood-tracker-tropical-village-nhs.vercel.app",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Admin routes
