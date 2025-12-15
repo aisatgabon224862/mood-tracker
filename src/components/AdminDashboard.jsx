b7bb0d46f64bad1c809d3e2f418de7b3d97fd2d8
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-12-15 13:55:01 +0800

    Update AdminDashboard.jsx

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -159,7 +159,7 @@ const AdminDashboard = () => {
                 {filteredMoods.map((item) => (
                   <tr key={item._id}>
                     <td>{item.name}</td>
-                    <td>{FormData.grade}</td>
+                    <td>{grade}</td>
                     <td>{item.section}</td>
                     <td>{item.explanation}</td>
                     <td>{new Date(item.createdAt).toLocaleDateString()}</td>
