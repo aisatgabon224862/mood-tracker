85b76f2c0ef25068193e261315b9bb59d4a30602
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-12-15 13:51:47 +0800

    Update selectedGrade display in AdminDashboard

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -159,7 +159,7 @@ const AdminDashboard = () => {
                 {filteredMoods.map((item) => (
                   <tr key={item._id}>
                     <td>{item.name}</td>
-                    <td>{item.selectedGrade}</td>
+                    <td>{selectedGrade}</td>
                     <td>{item.section}</td>
                     <td>{item.explanation}</td>
                     <td>{new Date(item.createdAt).toLocaleDateString()}</td>
