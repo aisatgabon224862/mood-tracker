88fe853c6e1d65eb416f92212115890f4a6232e3
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-12-15 13:09:37 +0800

    Update footer text in Home component
    Removed the 'forged by' text from the footer.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -47,7 +47,7 @@ function Home() {
           ))}
         </div>
 
-        <p className="small">© {new Date().getFullYear()} Mood Tracker <i>forged by:</i><b>Kurt Gabon</b></p>
+        <p className="small">© {new Date().getFullYear()} Mood Tracker</p> <i>forged by:</i><b> Kurt Gabon</b>
 
         <footer className="foot-er mb-3">
           <a
