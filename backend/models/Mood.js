72eb86646c7f2164a370ece49a2826c67f891e2c
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-12-15 15:01:07 +0800

    Update Mood.js

--- backend/models/Mood.js
+++ backend/models/Mood.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const moodSchema = new mongoose.Schema({
+  No: { type: number, required: false},
   name: { type: String, required: false },
   section: { type: String, required: true },
   grade: { type: String, required: true },
