77d3df6262973f06fb1011601a8e75d79bb18a85
aisatgabon224862 <aisat.gabon224862@gmail.com>
2025-12-16 20:09:18 +0800

    Update adminroutes.js

--- backend/routes/adminroutes.js
+++ backend/routes/adminroutes.js
@@ -7,15 +7,6 @@ import Mood from "../models/Mood.js";
 const router = express.Router();
 
 // Admin Login
-router.delete("/moods/delete-all",async(req,res)=>{
-  try{
-    await Mood.deleteMany({});
-    res.json({message:"All entry deleted succesfully"});
-  }catch (err){
-    res.status(500).json({message:"failed to delete all enrty"})
-  }
-})
-
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   console.log("Login attempt:", { email });
